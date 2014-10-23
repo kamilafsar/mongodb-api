87c25391f73a39812cf8259da03dedee27c5cc14
Kamil Afsar <kamil.afsar1@gmail.com>
2014-10-23 13:58:30 +0200

    added LogicalExpressions spec

---
+++ src/test/scala/mongodbapi/expression/LogicalExpressionsSpec.scala
@@ -0,0 +1,64 @@
+package mongodbapi.expression
+
+import org.scalacheck.Prop._
+import org.scalacheck.{Arbitrary, Properties}
+import reactivemongo.bson._
+
+object LogicalExpressionsSpec extends Properties("LogicalExpressions") {
+
+  import mongodbapi._
+
+  case class MyClass(myProperty: String)
+
+  implicit val myClassHandler = Macros.handler[MyClass]
+
+  trait MyClassMetadata extends DocumentTypeMetadata[MyClass] {
+    val myProperty = new Field[String, BSONString]("myProperty")
+  }
+
+  implicit object MyClassMetadata extends MyClassMetadata
+
+  implicit val genMyClass = Arbitrary[MyClass] {
+    for (myProperty <- Arbitrary.arbitrary[String])
+    yield MyClass(myProperty)
+  }
+
+  property("$and") = forAll { (fieldName1: String, fieldValue1: String,
+                               fieldName2: String, fieldValue2: MyClass,
+                               fieldName3: String, fieldValue3: Int) =>
+    val field1 = new Field[String, BSONString](fieldName1)
+    val field2 = new Field[MyClass, BSONDocument](fieldName2)
+    val field3 = new Field[Int, BSONInteger](fieldName3)
+
+    val expr = ((field1 $eq fieldValue1) && (field2 $eq fieldValue2) && (field3 $eq fieldValue3))
+
+    expr.toBSON == BSONDocument(
+      "$and" -> BSONArray(
+        BSONDocument(fieldName1 -> fieldValue1),
+        BSONDocument(fieldName2 -> fieldValue2),
+        BSONDocument(fieldName3 -> fieldValue3)
+      )
+    )
+  }
+
+  property("$or") = forAll { (fieldName1: String, fieldValue1: String,
+                               fieldName2: String, fieldValue2: MyClass,
+                               fieldName3: String, fieldValue3: Int) =>
+    val field1 = new Field[String, BSONString](fieldName1)
+    val field2 = new Field[MyClass, BSONDocument](fieldName2)
+    val field3 = new Field[Int, BSONInteger](fieldName3)
+
+    val expr = ((field1 $eq fieldValue1) || (field2 $eq fieldValue2) || (field3 $eq fieldValue3))
+
+    expr.toBSON == BSONDocument(
+      "$or" -> BSONArray(
+        BSONDocument(fieldName1 -> fieldValue1),
+        BSONDocument(fieldName2 -> fieldValue2),
+        BSONDocument(fieldName3 -> fieldValue3)
+      )
+    )
+  }
+
+  // TODO property("$nor")
+
+}
